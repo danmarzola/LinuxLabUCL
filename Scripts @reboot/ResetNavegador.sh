aa6c26b362ace9d60a6087f38d74539a959f1355
danmarzola <danmarzola@gmail.com>
2020-02-28 16:46:38 -0300

    Update ResetNavegador.sh

--- Scripts @reboot/ResetNavegador.sh
+++ Scripts @reboot/ResetNavegador.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo rm -rf '/home/google-chrome/' '/home/ucl/.config/' && sudo rm -rf '/home/ucl/.cache/google-chrome/' && sudo rm -rf '/home/ucl/.cache/mozilla/' && sudo rm -rf '/home/ucl/.mozilla/'
+sudo rm -rf '/home/ucl/.config/google-chrome/' && sudo rm -rf '/home/ucl/.cache/google-chrome/' && sudo rm -rf '/home/ucl/.cache/mozilla/' && sudo rm -rf '/home/ucl/.mozilla/'
