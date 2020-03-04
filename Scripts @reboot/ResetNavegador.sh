ac42de3acc2ca173abd6831e6a84e56e42a5c80a
danmarzola <danmarzola@gmail.com>
2020-03-04 16:12:46 -0300

    Update ResetNavegador.sh

--- Scripts @reboot/ResetNavegador.sh
+++ Scripts @reboot/ResetNavegador.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo rm -rf '/home/ucl/.config/google-chrome/' && sudo rm -rf '/home/ucl/.cache/google-chrome/' && sudo rm -rf '/home/ucl/.cache/mozilla/' && sudo rm -rf '/home/ucl/.mozilla/' && sudo rm /home/ucl/*
+sudo rm -rf '/home/ucl/.config/google-chrome/' && sudo rm -rf '/home/ucl/.cache/google-chrome/' && sudo rm -rf '/home/ucl/.cache/mozilla/' && sudo rm -rf '/home/ucl/.mozilla/' && sleep 20 && sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
